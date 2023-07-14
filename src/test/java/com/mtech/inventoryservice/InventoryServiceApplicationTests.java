060ab365d2ab7391a711ebf949a49f9df86d642c
Dean Minnie <dminnie33@gmail.com>
2023-07-14 10:04:00 +0200

    Added Unite tests

--- src/test/java/com/mtech/inventoryservice/InventoryServiceApplicationTests.java
+++ src/test/java/com/mtech/inventoryservice/InventoryServiceApplicationTests.java
@@ -1,10 +1,46 @@
 package com.mtech.inventoryservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mtech.inventoryservice.repository.InventoryRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
+@AutoConfigureMockMvc
 class InventoryServiceApplicationTests {
+
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest")
+			.withDatabaseName("mysql-integration-tests-db")
+			.withUsername("sa")
+			.withPassword("sa");
+
+	static {
+		mySQLContainer.start();
+	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private InventoryRepository inventoryRepository;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+		dynamicPropertyRegistry.add("spring.datasource.username", mySQLContainer::getUsername);
+		dynamicPropertyRegistry.add("spring.datasource.password", mySQLContainer::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
